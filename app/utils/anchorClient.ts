8ca97947659599e3718a51bd9ff4d892f8f6493b
swaparup36 <swaparup36@gmail.com>
2025-10-30 19:44:00 +0530

    custom rpc url added

--- app/utils/anchorClient.ts
+++ app/utils/anchorClient.ts
@@ -3,11 +3,13 @@ import { AnchorProvider, Program, Idl } from "@coral-xyz/anchor";
 import idl from "./trender_program.json";
 
 const PROGRAM_ID = new PublicKey("9ZFKHrBrA2YC19eLvuCM4kjabjXFqphYJs8PxgeeSG7S");
+const SOLANA_RPC_URL = process.env.NEXT_PUBLIC_SOLANA_RPC_URL || "https://api.devnet.solana.com";
+// console.log("SOLANA_RPC_URL: ", SOLANA_RPC_URL)
 
 export type TrenderProgram = Program<Idl>;
 
 export function getAnchorClient(wallet: any) {
-  const connection = new Connection("https://api.devnet.solana.com", {
+  const connection = new Connection(SOLANA_RPC_URL, {
     commitment: "confirmed",
     confirmTransactionInitialTimeout: 60000,
   });
